4f0f269394f5d1bd6936e27f74d6b47e51606f54
Marta <marta.osm00@gmail.com>
2022-03-17 11:14:22 +0100

    Adding sounds and death animation left.

---
+++ Game#2/Assets/Scripts/ItemCollector.cs
@@ -0,0 +1,42 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ItemCollector : MonoBehaviour
+{
+    private int apples = 0;
+    private int pineapples = 0;
+    private int melons = 0;
+    private int points = 0;
+    [SerializeField] private Text pointsText;
+    void Update()
+    {
+        OnTriggerEnter2D(GetComponent<Collider2D>());
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.CompareTag("Apple") || collision.gameObject.CompareTag("Pineapple") || collision.gameObject.CompareTag("Melon"))
+        {
+            Destroy(collision.gameObject);
+            if (collision.gameObject.CompareTag("Apple"))
+            {
+                apples++;
+                points++;
+            }
+            if (collision.gameObject.CompareTag("Pineapple"))
+            {
+                pineapples = pineapples + 10;
+                points = points + 10;
+            }
+            if (collision.gameObject.CompareTag("Melon"))
+            {
+                melons = melons + 20;
+                points = points + 20;
+            }
+
+            pointsText.text = "Points: " + points;
+        }
+    }
+}
